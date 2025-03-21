3a1b121e0635d1f5f56e09706ab070a59964bd33
陶浩南 <taoaaron5@gmail.com>
2025-03-21 20:45:42 +1100

    customize colors
    customize colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,15 @@
 /*
  * @Date: 2025-03-21 20:33:40
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-21 20:36:03
+ * @LastEditTime: 2025-03-21 20:45:36
  * @FilePath: /Money_Recorder/app/index.tsx
  */
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Text>The Money_Recorder</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold"> Money_Recorder</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      // customize colors
+      colors: {
+        primary: "#007AFF",
+        secondary: "#606060",
+        tertiary: "#F5F5F5",
+        quaternary: "#E0E0E0",
+        quinary: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
