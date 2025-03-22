7c84c1cdaf7550a702e1a00bdac9d8e7f8ba05e5
陶浩南 <taoaaron5@gmail.com>
2025-03-22 20:00:47 +1100

    the login and sign up switch

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-03-21 20:33:40
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-22 19:54:09
+ * @LastEditTime: 2025-03-22 19:59:46
  * @FilePath: /Money_Recorder/app/index.tsx
  */
 import { useState } from "react";
@@ -33,6 +33,7 @@ export default function Index() {
 
   return (
     <View className="flex-1 justify-center items-center bg-white p-6">
+      {/* 主题和图片+介绍 */}
       <Image source={require("../assets/images/HomeTitle.png")} />
       <Text className="text-3xl font-bold text-center text-secondary mb-16 mt-6 justify-center">
         The Money-Recorder
@@ -41,34 +42,12 @@ export default function Index() {
         Start to record the money you spent and earned every day.
       </Text>
       <View className="w-full max-w-sm">
-        {/* 标题 */}
+        {/* 切换显示*/}
         <Text className="text-3xl font-bold text-center text-quaternary mb-8">
-          {isLogin ? "Log in" : "Create the Account"}
+          {isLogin ? "Log-In" : "Sign-Up"}
         </Text>
 
-        {/* 切换按钮 */}
-        <View className="flex-row justify-center mb-8">
-          <TouchableOpacity
-            onPress={() => setIsLogin(true)}
-            className={`px-6 py-2 rounded-l-full ${
-              isLogin ? "bg-primary" : "bg-gray-200"
-            }`}>
-            <Text className={isLogin ? "text-white" : "text-gray-600"}>
-              登录
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setIsLogin(false)}
-            className={`px-6 py-2 rounded-r-full ${
-              !isLogin ? "bg-primary" : "bg-gray-200"
-            }`}>
-            <Text className={!isLogin ? "text-white" : "text-gray-600"}>
-              注册
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* 表单 */}
+        {/* 用户名密码表单 */}
         <View className="space-y-4">
           <View className="relative">
             <Ionicons
@@ -127,6 +106,24 @@ export default function Index() {
             </Text>
           </TouchableOpacity>
         </View>
+        {/* 切换按钮 */}
+        {isLogin ? (
+          <View className="mt-6 flex-row justify-center mb-8">
+            <TouchableOpacity onPress={() => setIsLogin(false)}>
+              <Text className={!isLogin ? "text-quaternary" : "text-secondary"}>
+                Don't have an account?
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View className="mt-6 flex-row justify-center mb-8">
+            <TouchableOpacity onPress={() => setIsLogin(true)}>
+              <Text className={isLogin ? "text-quaternary" : "text-secondary"}>
+                Already have an account?
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </View>
   );
