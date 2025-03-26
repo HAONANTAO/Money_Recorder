281f8d3f32dbe1e85c0e05b383769f94410c8b51
陶浩南 <taoaaron5@gmail.com>
2025-03-26 13:10:39 +1100

    start to build interfaces and database

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -13,3 +13,17 @@ interface User {
   // AWS S3 存储用户头像的路径？？
   avatar: string; // 用户头像，类型为 string，通常是一个 URL
 }
+
+interface Record {
+  $id: string;
+  userId: string; // 关联的用户ID
+  createAt: string; // 记录创建时间
+  moneyAmount: number; // 金额
+  type: "income" | "expense"; // 收入或支出
+  category: string; // 收支类别（如餐饮、交通等）
+  paymentMethod: string; // 支付方式（如现金、信用卡等）
+  tags: string[]; // 自定义标签
+  location: string; // 消费地点 ！
+  recurring: boolean; // 是否为周期性收支 ！
+  comment: string; // 备注说明
+}
