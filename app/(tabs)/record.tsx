1ef4f846d0033dd9bf6e8e1f7b40733a8c16a191
陶浩南 <taoaaron5@gmail.com>
2025-03-26 15:18:22 +1100

    beauty the record page1

--- app/(tabs)/record.tsx
+++ app/(tabs)/record.tsx
@@ -14,22 +14,22 @@ import { StorageService } from "@/utils/storageService";
 import { getUserByEmail } from "@/services/userManagement";
 
 const EXPENSE_CATEGORIES = [
-  { label: "餐饮", value: "餐饮" },
-  { label: "交通", value: "交通" },
-  { label: "购物", value: "购物" },
-  { label: "娱乐", value: "娱乐" },
-  { label: "居住", value: "居住" },
-  { label: "医疗", value: "医疗" },
-  { label: "教育", value: "教育" },
-  { label: "其他", value: "其他" },
+  { label: "Eating", value: "eating" },
+  { label: "Traffic", value: "traffic" },
+  { label: "Shopping", value: "shopping" },
+  { label: "Entertainment", value: "entertainment" },
+  { label: "Living", value: "living" },
+  { label: "Medication", value: "medication" },
+  { label: "Education", value: "education" },
+  { label: "Others", value: "others" },
 ];
 
 const INCOME_CATEGORIES = [
-  { label: "工资", value: "工资" },
-  { label: "副业", value: "副业" },
-  { label: "投资", value: "投资" },
-  { label: "奖金", value: "奖金" },
-  { label: "其他", value: "其他" },
+  { label: "Salary", value: "salary" },
+  { label: "Sideline", value: "sideline" },
+  { label: "Investment", value: "investment" },
+  { label: "Bonus", value: "bonus" },
+  { label: "Other", value: "other" },
 ];
 
 const Record = () => {
@@ -39,6 +39,7 @@ const Record = () => {
   const [record, setRecord] = useState({
     userId: "",
     moneyAmount: "",
+    // ，'as'是类型断言操作符。这里'expense' as 'income' | 'expense'的作用是告诉TypeScript编译器将'expense'字符串值明确地视为'income'或'expense'联合类型中的一个。
     type: "expense" as "income" | "expense",
     category: "",
     paymentMethod: "",
@@ -58,7 +59,7 @@ const Record = () => {
           setRecord((prev) => ({ ...prev, userId: userInfo.$id }));
         }
       } catch (error) {
-        console.error("获取用户ID失败:", error);
+        console.error("Get UserID failed:", error);
       }
     };
     getUserId();
@@ -67,7 +68,7 @@ const Record = () => {
   const handleSubmit = async () => {
     try {
       if (!record.moneyAmount || !record.category) {
-        alert("请填写金额和类别");
+        alert("Pleas filled amount and category");
         return;
       }
       const newRecord = await createRecord({
@@ -75,7 +76,7 @@ const Record = () => {
         moneyAmount: parseFloat(record.moneyAmount),
         tags: record.tags.split(",").filter(Boolean).join(","),
       });
-      alert("记录创建成功");
+      alert("record create successfully");
       // 清空表单
       setRecord({
         ...record,
@@ -87,8 +88,8 @@ const Record = () => {
         comment: "",
       });
     } catch (error) {
-      console.error("创建记录失败:", error);
-      alert("创建记录失败，请重试");
+      console.error("failed create record:", error);
+      alert("failed create record,try again");
     }
   };
 
@@ -97,10 +98,10 @@ const Record = () => {
       className={`flex-1 ${isDark ? "bg-quaternary" : "bg-gray-100"}`}>
       <View className="p-5">
         <Text
-          className={`text-2xl font-bold mb-5 text-center ${
-            isDark ? "text-gray-200" : "text-gray-800"
+          className={`mt-12 text-2xl font-bold mb-5 text-center ${
+            isDark ? "text-secondary" : "text-quaternary"
           }`}>
-          记账
+          Add Record
         </Text>
 
         {/* 金额输入 */}
@@ -112,19 +113,16 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            金额
+            Amount
           </Text>
-          <View className="flex-row items-center p-3 bg-white rounded-lg">
+          <View className={`flex-row items-center p-3 bg-white rounded-lg`}>
             <Text
               className={`text-xl mr-2 font-medium ${
                 isDark ? "text-gray-200" : "text-gray-700"
               }`}>
-              ¥
+              $
             </Text>
             <TextInput
-              className={`flex-1 text-base ${
-                isDark ? "text-gray-200 bg-gray-700" : "text-gray-800 bg-white"
-              }`}
               placeholder="0.00"
               placeholderTextColor={isDark ? "#9CA3AF" : "#6B7280"}
               keyboardType="numeric"
@@ -145,9 +143,12 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            类型
+            Type
           </Text>
-          <View className="overflow-hidden bg-white rounded-lg">
+          <View
+            className={`${
+              isDark ? "" : "bg-white"
+            }overflow-hidden  rounded-lg`}>
             <Picker
               selectedValue={record.type}
               onValueChange={(value) => setRecord({ ...record, type: value })}
@@ -155,12 +156,12 @@ const Record = () => {
                 isDark ? "bg-gray-700 text-gray-200" : "bg-white text-gray-800"
               }`}>
               <Picker.Item
-                label="支出"
+                label="Expense"
                 value="expense"
                 color={isDark ? "#EF4444" : "#DC2626"}
               />
               <Picker.Item
-                label="收入"
+                label="Income"
                 value="income"
                 color={isDark ? "#10B981" : "#059669"}
               />
@@ -177,9 +178,12 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            类别
+            Category
           </Text>
-          <View className="overflow-hidden bg-white rounded-lg">
+          <View
+            className={`${
+              isDark ? "" : "bg-white"
+            }overflow-hidden  rounded-lg`}>
             <Picker
               selectedValue={record.category}
               onValueChange={(value) =>
@@ -189,8 +193,8 @@ const Record = () => {
                 isDark ? "bg-gray-700 text-gray-200" : "bg-white text-gray-800"
               }`}>
               <Picker.Item
-                label="选择类别"
-                value=""
+                label="Choose category"
+                value="Choose category"
                 color={isDark ? "#9CA3AF" : "#6B7280"}
               />
               {(record.type === "expense"
@@ -217,13 +221,13 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            支付方式
+            Payment method
           </Text>
           <TextInput
             className={`p-3 rounded-lg text-base ${
               isDark ? "bg-gray-700 text-gray-200" : "bg-white text-gray-800"
             }`}
-            placeholder="现金/支付宝/微信"
+            placeholder="Credit/Debit/Cash"
             placeholderTextColor={isDark ? "#9CA3AF" : "#6B7280"}
             value={record.paymentMethod}
             onChangeText={(value) =>
@@ -241,13 +245,13 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            地点
+            Location
           </Text>
           <TextInput
             className={`p-3 rounded-lg text-base ${
               isDark ? "bg-gray-700 text-gray-200" : "bg-white text-gray-800"
             }`}
-            placeholder="输入消费地点"
+            placeholder="Enter location"
             placeholderTextColor={isDark ? "#9CA3AF" : "#6B7280"}
             value={record.location}
             onChangeText={(value) => setRecord({ ...record, location: value })}
@@ -263,13 +267,13 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            标签
+            Labels
           </Text>
           <TextInput
             className={`p-3 rounded-lg text-base ${
               isDark ? "bg-gray-700 text-gray-200" : "bg-white text-gray-800"
             }`}
-            placeholder="多个标签用逗号分隔"
+            placeholder="multiple tags separated by commas"
             placeholderTextColor={isDark ? "#9CA3AF" : "#6B7280"}
             value={record.tags}
             onChangeText={(value) => setRecord({ ...record, tags: value })}
@@ -285,13 +289,13 @@ const Record = () => {
             className={`mb-2 text-base font-medium ${
               isDark ? "text-gray-200" : "text-gray-700"
             }`}>
-            备注
+            Comments
           </Text>
           <TextInput
             className={`p-3 rounded-lg text-base ${
               isDark ? "bg-gray-700 text-gray-200" : "bg-white text-gray-800"
             }`}
-            placeholder="添加备注..."
+            placeholder="add comment..."
             placeholderTextColor={isDark ? "#9CA3AF" : "#6B7280"}
             value={record.comment}
             onChangeText={(value) => setRecord({ ...record, comment: value })}
@@ -307,7 +311,7 @@ const Record = () => {
             isDark ? "bg-blue-700" : "bg-blue-500"
           }`}>
           <Text className="text-base font-semibold text-center text-white">
-            保存记录
+            Save Record
           </Text>
         </TouchableOpacity>
       </View>
