656c26787a468d48a441c2e2ad4882dafb51e25f
陶浩南 <taoaaron5@gmail.com>
2025-03-28 18:04:04 +1100

    set budget details

--- app/(func)/budgetDetail.tsx
+++ app/(func)/budgetDetail.tsx
@@ -1,8 +1,9 @@
-import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import { Text, View, TouchableOpacity, TextInput, Alert } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useTheme } from "../../contexts/ThemeContext";
 import { deleteBudget, updateBudget } from "@/services/budgetService";
+import { Ionicons } from "@expo/vector-icons";
 
 const BudgetDetail = () => {
   const { theme } = useTheme();
@@ -19,33 +20,66 @@ const BudgetDetail = () => {
   }, [amount]);
 
   const handleDelete = async () => {
-    try {
-      await deleteBudget(budgetId as string);
-      router.back();
-    } catch (error) {
-      console.error("删除预算失败:", error);
-    }
+    Alert.alert(
+      "确认删除",
+      "您确定要删除这个预算吗？",
+      [
+        {
+          text: "取消",
+          onPress: () => console.log("删除操作已取消"),
+          style: "cancel",
+        },
+        {
+          text: "确定",
+          onPress: async () => {
+            try {
+              await deleteBudget(budgetId as string);
+              router.back();
+            } catch (error) {
+              console.error("删除预算失败:", error);
+            }
+          },
+        },
+      ],
+      { cancelable: false },
+    );
   };
 
   const handleUpdate = () => {
     if (isEditing && newAmount !== String(amount)) {
-      // Construct the updated data object
-      const updatedData = {
-        category: category as string,
-        amount: parseFloat(newAmount), // Update the amount
-      };
-      console.log("更新预算，ID:", budgetId);
+      Alert.alert(
+        "确认更新",
+        "您确定要保存这些更改吗？",
+        [
+          {
+            text: "取消",
+            onPress: () => console.log("更新操作已取消"),
+            style: "cancel",
+          },
+          {
+            text: "确定",
+            onPress: () => {
+              const updatedData = {
+                category: category as string,
+                amount: parseFloat(newAmount), // Update the amount
+              };
+              console.log("更新预算，ID:", budgetId);
 
-      // Call the updateBudget function with the budgetId and updatedData
-      updateBudget(budgetId as string, updatedData)
-        .then(() => {
-          // After successful update, save the new amount and switch off editing mode
-          setIsEditing(false);
-          setNewAmount(String(updatedData.amount)); // Update the state with the new amount
-        })
-        .catch((error) => {
-          console.error("更新预算失败:", error);
-        });
+              // Call the updateBudget function with the budgetId and updatedData
+              updateBudget(budgetId as string, updatedData)
+                .then(() => {
+                  // After successful update, save the new amount and switch off editing mode
+                  setIsEditing(false);
+                  setNewAmount(String(updatedData.amount)); // Update the state with the new amount
+                })
+                .catch((error) => {
+                  console.error("更新预算失败:", error);
+                });
+            },
+          },
+        ],
+        { cancelable: false },
+      );
     } else {
       setIsEditing(true); // Switch to edit mode
     }
@@ -106,6 +140,27 @@ const BudgetDetail = () => {
           <Text className="text-lg font-semibold text-white">删除预算</Text>
         </TouchableOpacity>
       </View>
+
+      {/* 如果预算为0，显示 Set Budget 按钮 */}
+      {parseFloat(newAmount) === 0 && (
+        <TouchableOpacity
+          onPress={() => router.push("/(func)/budget")}
+          className={`flex-row items-center p-3 mt-6 rounded-xl shadow-md ${
+            theme === "dark" ? "bg-quaternary" : "bg-white"
+          }`}>
+          <Ionicons
+            name="wallet-outline"
+            size={20}
+            color={theme === "dark" ? "#1477f1" : "#0d6df4"}
+          />
+          <Text
+            className={`ml-4 text-lg font-semibold ${
+              theme === "dark" ? "text-gray-200" : "text-gray-700"
+            }`}>
+            设置预算
+          </Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
--- app/(tabs)/goals.tsx
+++ app/(tabs)/goals.tsx
@@ -4,6 +4,7 @@ import {
   View,
   TouchableOpacity,
   ScrollView,
+  RefreshControl,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
@@ -18,54 +19,65 @@ import { getMonthlyExpensesByCategory } from "@/services/recordService";
 const Goals = () => {
   const router = useRouter();
   const { theme } = useTheme();
-  const [userId, setUserId] = useState<string | null>(null); // Set userId as string | null
+
+  const [userId, setUserId] = useState<string | null>(null);
   const [monthlyBudgets, setMonthlyBudgets] = useState<any>(null);
   const [expensesByCategory, setExpensesByCategory] = useState<any>(null);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchData = async () => {
+    setIsRefreshing(true);
+    try {
+      // 获取用户邮箱并获取用户信息
+      const email = await StorageService.getEmail();
+      if (!email) {
+        console.error("No email found in storage");
+        return;
+      }
+
+      const user = await getUserByEmail(email);
+      const userId = user.$id as string;
+      setUserId(userId);
+
+      // 获取用户预算
+      const currentYear = new Date().getFullYear();
+      const currentMonth = new Date().getMonth() + 1;
+      const budgets = await getMonthlyBudget(userId, currentYear, currentMonth);
+      setMonthlyBudgets(budgets);
+
+      // 获取按类别的支出
+      const result = await getMonthlyExpensesByCategory(
+        userId,
+        currentYear,
+        currentMonth,
+      );
+      setExpensesByCategory(result);
+    } catch (error) {
+      console.error("获取预算失败:", error);
+    } finally {
+      setIsRefreshing(false); // 刷新结束
+    }
+  };
 
   useEffect(() => {
-    const getInitialUserAndFetchBudgets = async () => {
-      try {
-        // Step 1: Get user email and fetch user details
-        const email = await StorageService.getEmail();
-        if (!email) {
-          console.error("No email found in storage");
-          return;
-        }
-
-        const user = await getUserByEmail(email);
-        const userId = user.$id as string;
-        setUserId(userId);
-
-        // Step 2: Fetch budgets once userId is available
-        const currentYear = new Date().getFullYear();
-        const currentMonth = new Date().getMonth() + 1;
-        const budgets = await getMonthlyBudget(
-          userId,
-          currentYear,
-          currentMonth,
-        );
-        setMonthlyBudgets(budgets);
-
-        const result = await getMonthlyExpensesByCategory(
-          userId,
-          currentYear,
-          currentMonth,
-        );
-        setExpensesByCategory(result);
-      } catch (error) {
-        console.error("获取预算失败:", error);
-      }
-    };
-
-    getInitialUserAndFetchBudgets();
-  }, []); // Empty array ensures this runs only once on component mount
+    fetchData();
+  }, []); // 只在组件挂载时执行一次
 
   return (
     <View
       className={`flex-1 ${
         theme === "dark" ? "bg-quaternary" : "bg-gray-100"
       }`}>
-      <ScrollView className="px-6 py-10">
+      <ScrollView
+        className="px-6 py-10"
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={fetchData} // 触发刷新时重新获取数据
+            tintColor={theme === "dark" ? "#1477f1" : "#0d6df4"} // 设置刷新指示器的颜色
+            progressBackgroundColor={theme === "dark" ? "#333" : "#fff"} // 设置刷新指示器背景色
+          />
+        }>
         <Text
           className={`mb-8 text-3xl font-extrabold text-center ${
             theme === "dark" ? "text-gray-200" : "text-gray-800"
@@ -85,7 +97,6 @@ const Goals = () => {
               const budget = monthlyBudgets?.find(
                 (b: any) => b.category === category.value,
               ) || { budgetAmount: 0 };
-
               const expense = expensesByCategory?.[category.value] || 0;
 
               return (
@@ -93,7 +104,6 @@ const Goals = () => {
                   key={category.value}
                   onPress={() => {
                     const budgetId = budget?.budgetId;
-
                     router.push({
                       pathname: "/(func)/budgetDetail",
                       params: {
