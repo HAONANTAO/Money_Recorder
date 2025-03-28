f1857e860650985f4a74b68ce7aad797f174c4e6
陶浩南 <taoaaron5@gmail.com>
2025-03-28 14:03:00 +1100

    build the goal page1

--- app/(tabs)/goals.tsx
+++ app/(tabs)/goals.tsx
@@ -1,9 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "../../contexts/ThemeContext";
+import { getUserByEmail } from "@/services/userManagement";
+import { StorageService } from "@/utils/storageService";
 
 const Goals = () => {
   const { theme } = useTheme();
+  const [userId, setUserId] = useState(null);
+  useEffect(() => {
+    const getInitialUser = async () => {
+      const email = await StorageService.getEmail();
+      const user = await getUserByEmail(email as unknown as string);
+      setUserId(user.$id as unknown as null);
+      console.log("这里的user是：", userId);
+    };
+    getInitialUser();
+  }, []);
   return (
     <View
       className={`flex-1 justify-center items-center ${
