24f5253d214866da3499d9389a411330d9669ded
陶浩南 <taoaaron5@gmail.com>
2025-03-30 12:28:53 +1100

    the important search bar for tag and category

---
+++ app/(func)/searchbar.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Date: 2025-03-30 12:28:24
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-03-30 12:28:27
+ * @FilePath: /Money_Recorder/app/(func)/searchbar.tsx
+ */
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Searchbar = () => {
+  return (
+    <View>
+      <Text>searchbar</Text>
+    </View>
+  );
+};
+
+export default Searchbar;
+
+const styles = StyleSheet.create({});
