a7745b0c741e191450d522faa45080bebb8549b2
陶浩南 <taoaaron5@gmail.com>
2025-04-04 20:01:32 +1100

    the keyboard avoiding view

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-03-21 20:33:40
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-02 13:20:46
+ * @LastEditTime: 2025-04-04 20:00:52
  * @FilePath: /Money_Recorder/app/index.tsx
  */
 import { useEffect, useState } from "react";
@@ -18,6 +18,7 @@ import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { createUser, loginUser } from "@/services/userManagement";
 import { StorageService } from "@/utils/storageService";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 export default function Index() {
   // 默认登录状态
@@ -134,62 +135,84 @@ export default function Index() {
   };
 
   return (
-    <View className="flex-1 justify-center items-center p-6 bg-white">
-      {/* 背景图片 */}
-      <View className="absolute inset-0 z-0">
-        <Image
-          source={require("../assets/images/Background.png")}
-          className="w-full h-full"
-          resizeMode="cover"
-        />
-      </View>
-      {/* 主题和图片+介绍 */}
-      <Image source={require("../assets/images/HomeTitle.png")} />
-      <Text className="justify-center mt-6 mb-16 text-3xl font-bold text-center text-secondary">
-        The Money-Recorder
-      </Text>
-      <Text className="justify-center mb-12 text-lg font-extrabold text-center text-gray-300">
-        Start to record the money you spent and earned every day.
-      </Text>
-
-      {/* 主要body */}
-      <View className="w-full max-w-sm">
-        {/* 切换显示*/}
-        <Text className="mb-8 text-3xl font-bold text-center text-quaternary">
-          {isLogin ? "Log-In" : "Sign-Up"}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}>
+      <View className="flex-1 justify-center items-center p-6 bg-white">
+        {/* 背景图片 */}
+        <View className="absolute inset-0 z-0">
+          <Image
+            source={require("../assets/images/Background.png")}
+            className="w-full h-full"
+            resizeMode="cover"
+          />
+        </View>
+        {/* 主题和图片+介绍 */}
+        <Image source={require("../assets/images/HomeTitle.png")} />
+        <Text className="justify-center mt-6 mb-16 text-3xl font-bold text-center text-secondary">
+          The Money-Recorder
+        </Text>
+        <Text className="justify-center mb-12 text-lg font-extrabold text-center text-gray-300">
+          Start to record the money you spent and earned every day.
         </Text>
 
-        {/* 用户名密码表单 */}
-        <View className="space-y-4">
-          <View className="relative mb-4">
-            <Ionicons
-              name="person-outline"
-              size={20}
-              color="#9CA3AF"
-              style={{ position: "absolute", left: 12, top: 12 }}
-            />
-            <TextInput
-              placeholder="Email..."
-              placeholderTextColor="#9CA3AF" // 添加这行来设置 placeholder 的颜色
-              value={email}
-              onChangeText={(text) => {
-                setEmail(text);
-                setIsValidEmail(validateEmail(text));
-              }}
-              className="py-3 pr-4 pl-10 text-gray-800 bg-gray-100 rounded-lg"
-              // 自动填充禁用
-              autoComplete="off"
-              textContentType="none"
-              keyboardType="email-address"
-            />
-            {!isValidEmail && email.length > 0 && (
-              <Text className="mt-1 text-sm text-red-500">
-                Please enter a valid email address
-              </Text>
-            )}
-          </View>
+        {/* 主要body */}
+        <View className="w-full max-w-sm">
+          {/* 切换显示*/}
+          <Text className="mb-8 text-3xl font-bold text-center text-quaternary">
+            {isLogin ? "Log-In" : "Sign-Up"}
+          </Text>
 
-          {!isLogin && (
+          {/* 用户名密码表单 */}
+          <View className="space-y-4">
+            <View className="relative mb-4">
+              <Ionicons
+                name="person-outline"
+                size={20}
+                color="#9CA3AF"
+                style={{ position: "absolute", left: 12, top: 12 }}
+              />
+              <TextInput
+                placeholder="Email..."
+                placeholderTextColor="#9CA3AF" // 添加这行来设置 placeholder 的颜色
+                value={email}
+                onChangeText={(text) => {
+                  setEmail(text);
+                  setIsValidEmail(validateEmail(text));
+                }}
+                className="py-3 pr-4 pl-10 text-gray-800 bg-gray-100 rounded-lg"
+                // 自动填充禁用
+                autoComplete="off"
+                textContentType="none"
+                keyboardType="email-address"
+              />
+              {!isValidEmail && email.length > 0 && (
+                <Text className="mt-1 text-sm text-red-500">
+                  Please enter a valid email address
+                </Text>
+              )}
+            </View>
+
+            {!isLogin && (
+              <View className="relative mb-4">
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color="#9CA3AF"
+                  style={{ position: "absolute", left: 12, top: 12 }}
+                />
+                <TextInput
+                  placeholder="Username..."
+                  placeholderTextColor="#9CA3AF" // 添加这行来设置 placeholder 的颜色
+                  value={username}
+                  onChangeText={setUsername}
+                  className="py-3 pr-4 pl-10 text-gray-800 bg-gray-100 rounded-lg"
+                  autoComplete="off"
+                  textContentType="none"
+                />
+              </View>
+            )}
             <View className="relative mb-4">
               <Ionicons
                 name="lock-closed-outline"
@@ -198,107 +221,90 @@ export default function Index() {
                 style={{ position: "absolute", left: 12, top: 12 }}
               />
               <TextInput
-                placeholder="Username..."
+                placeholder="Password..."
                 placeholderTextColor="#9CA3AF" // 添加这行来设置 placeholder 的颜色
-                value={username}
-                onChangeText={setUsername}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
                 className="py-3 pr-4 pl-10 text-gray-800 bg-gray-100 rounded-lg"
                 autoComplete="off"
                 textContentType="none"
               />
             </View>
-          )}
-          <View className="relative mb-4">
-            <Ionicons
-              name="lock-closed-outline"
-              size={20}
-              color="#9CA3AF"
-              style={{ position: "absolute", left: 12, top: 12 }}
-            />
-            <TextInput
-              placeholder="Password..."
-              placeholderTextColor="#9CA3AF" // 添加这行来设置 placeholder 的颜色
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-              className="py-3 pr-4 pl-10 text-gray-800 bg-gray-100 rounded-lg"
-              autoComplete="off"
-              textContentType="none"
-            />
-          </View>
 
-          {/* 按钮触发 */}
+            {/* 按钮触发 */}
+            <TouchableOpacity
+              onPress={handleSubmit}
+              className="py-3 mt-6 rounded-lg bg-tertiary"
+              disabled={loading}>
+              {loading ? (
+                // 加载器
+                <ActivityIndicator color="white" />
+              ) : (
+                <Text className="font-semibold text-center text-white">
+                  {isLogin ? "LogIn" : "SignUp"}
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
+          {/* 切换按钮 */}
+          {isLogin ? (
+            <View className="flex-row justify-center mt-6 mb-8">
+              <TouchableOpacity onPress={() => setIsLogin(false)}>
+                <Text className="text-secondary">Don't have an account?</Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View className="flex-row justify-center mt-6 mb-8">
+              <TouchableOpacity onPress={() => setIsLogin(true)}>
+                <Text className="text-secondary">Already have an account?</Text>
+              </TouchableOpacity>
+            </View>
+          )}
           <TouchableOpacity
-            onPress={handleSubmit}
-            className="py-3 mt-6 rounded-lg bg-tertiary"
-            disabled={loading}>
-            {loading ? (
-              // 加载器
-              <ActivityIndicator color="white" />
-            ) : (
-              <Text className="font-semibold text-center text-white">
-                {isLogin ? "LogIn" : "SignUp"}
-              </Text>
-            )}
+            onPress={handleGuestLogin}
+            className="mt-4 text-center">
+            <Text className="text-sm text-gray-600">Continue as Guest</Text>
           </TouchableOpacity>
         </View>
-        {/* 切换按钮 */}
-        {isLogin ? (
-          <View className="flex-row justify-center mt-6 mb-8">
-            <TouchableOpacity onPress={() => setIsLogin(false)}>
-              <Text className="text-secondary">Don't have an account?</Text>
-            </TouchableOpacity>
-          </View>
-        ) : (
-          <View className="flex-row justify-center mt-6 mb-8">
-            <TouchableOpacity onPress={() => setIsLogin(true)}>
-              <Text className="text-secondary">Already have an account?</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-        <TouchableOpacity
-          onPress={handleGuestLogin}
-          className="mt-4 text-center">
-          <Text className="text-sm text-gray-600">Continue as Guest</Text>
-        </TouchableOpacity>
-      </View>
 
-      {/* 成功提示Modal */}
-      <Modal
-        animationType="fade"
-        transparent={true}
-        // 可见性被控制
-        visible={showSuccessModal}
-        onRequestClose={() => setShowSuccessModal(false)}>
-        <View className="flex-1 justify-center items-center bg-black/50">
-          <View className="items-center p-6 w-4/5 bg-white rounded-2xl">
-            <Ionicons name="checkmark-circle" size={60} color="#10B981" />
-            <Text className="mt-4 mb-6 text-xl font-bold text-center">
-              {isLogin ? "Login successfully!" : "SignUp successfully!"}
-            </Text>
-            <Text className="mb-6 text-center text-gray-600">
-              {isLogin
-                ? `Redirecting to the Main page in ${countdown}s...`
-                : "Please proceed to login"}
-            </Text>
-
-            <TouchableOpacity
-              onPress={() => {
-                setShowSuccessModal(false);
-                if (isLogin) {
-                  router.replace("/(tabs)/home");
-                } else {
-                  setIsLogin(true);
-                }
-              }}
-              className="px-8 py-3 w-full rounded-lg bg-tertiary">
-              <Text className="font-semibold text-center text-white">
-                Confirm
+        {/* 成功提示Modal */}
+        <Modal
+          animationType="fade"
+          transparent={true}
+          // 可见性被控制
+          visible={showSuccessModal}
+          onRequestClose={() => setShowSuccessModal(false)}>
+          <View className="flex-1 justify-center items-center bg-black/50">
+            <View className="items-center p-6 w-4/5 bg-white rounded-2xl">
+              <Ionicons name="checkmark-circle" size={60} color="#10B981" />
+              <Text className="mt-4 mb-6 text-xl font-bold text-center">
+                {isLogin ? "Login successfully!" : "SignUp successfully!"}
               </Text>
-            </TouchableOpacity>
+              <Text className="mb-6 text-center text-gray-600">
+                {isLogin
+                  ? `Redirecting to the Main page in ${countdown}s...`
+                  : "Please proceed to login"}
+              </Text>
+
+              <TouchableOpacity
+                onPress={() => {
+                  setShowSuccessModal(false);
+                  if (isLogin) {
+                    router.replace("/(tabs)/home");
+                  } else {
+                    setIsLogin(true);
+                  }
+                }}
+                className="px-8 py-3 w-full rounded-lg bg-tertiary">
+                <Text className="font-semibold text-center text-white">
+                  Confirm
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      </Modal>
-    </View>
+        </Modal>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
